2177b3f1d9f87eca1db3a858df8efbdb5463e1a4
Victoria Tillero <victoriatillero6@gmail.com>
2025-06-19 20:02:48 -0400

    5/5 comp

--- exercises.py
+++ exercises.py
@@ -138,15 +138,28 @@ weather_advice()
 
 def determine_season():
     # Your control flow logic goes here
-    month_input = input('Enter the month of the year (Jan - Dec)')
-    day_input = int(input('Enter day of the month: '))
+    month_input = input('Enter the month of the year (Jan - Dec):')
+    day_input= (input('Enter day of the month: '))
 
     if not day_input.isdigit():
         print('Invalid day, please enter a number')
         return
 
     day =int(day_input)
+    month = month_input.capitalize()
 
-    valid_months
-# Call the function
+    if (month == "Dec" and day >= 21) or (month in ["Jan","Feb",]) or (month == "Mar" and day <= 19):
+        season = "Winter"
+    elif (month == "Mar" and day >= 20) or (month in ["Apr","May",]) or (month == "Jun" and day <= 20):
+        season = "Spring"
+    elif (month == "Jun" and day >= 21) or (month in ["Jul","Aug",]) or (month == "Sept" and day <= 21):
+        season = "Summer"
+    elif (month == "Sep" and day >= 22) or (month in ["Oct","Nov",]) or (month == "Dec" and day <= 20):
+        season = "Fall"
+    else:
+        print("Invalid date")
+        return
+
+    print(f"{month} {day} is in {season}.")
+
 determine_season()
